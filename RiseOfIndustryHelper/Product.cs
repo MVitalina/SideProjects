5a9dd57a4b20f69a954ab207bcaf66d0aae708fa
Vitalina Miroshnyk <vitalinamiroshnyk@gmail.com>
2023-09-10 23:25:20 +0300

    ROI: parse db.json

---
+++ RiseOfIndustryHelper/Product.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseOfIndustryHelper
+{
+    public partial class Product
+    {
+        [JsonProperty("Product")]
+        public string ProductProduct { get; set; }
+
+        [JsonProperty("Building")]
+        public string Building { get; set; }
+
+        [JsonProperty("Level")]
+        public string Level { get; set; }
+
+        [JsonProperty("Worktime")]
+        public int Worktime { get; set; }
+
+        [JsonProperty("OutputQuantity")]
+        public int OutputQuantity { get; set; }
+
+        [JsonProperty("Input1")]
+        public string Input1 { get; set; }
+
+        [JsonProperty("Input1_Quantity")]
+        public int Input1Quantity { get; set; }
+
+        [JsonProperty("Input2")]
+        public string Input2 { get; set; }
+
+        [JsonProperty("Input2_Quantity")]
+        public int Input2Quantity { get; set; }
+
+        [JsonProperty("Input3")]
+        public string Input3 { get; set; }
+
+        [JsonProperty("Input3_Quantity")]
+        public int Input3Quantity { get; set; }
+    }
+}
